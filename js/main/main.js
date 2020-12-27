8cea084c60904852a08356f6fb9afbef46c8869e
Coospir <eugene.starodubov@gmail.com>
2020-12-27 17:47:10 +0300

    updated

---
+++ js/main/main.js
@@ -0,0 +1,76 @@
+
+import dataObject from "../utils/dataObject.js"
+import getOrders from "../connector/connector.js";
+
+window.onload = () => {
+    fetch(dataObject.getOrdersLink, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    }).then(response => response.json())
+        .then((data) => {
+            (data.response).forEach(item => {
+                document.querySelector(".cards").insertAdjacentHTML("afterbegin", `
+                <div class="card_item">
+                    <div>Order №<span class="id">${item.order_id}</span></div> 
+                    <hr>
+                    <p><b>Buyer:</b> ${item.name}</p>
+                    <p><b>Cost:</b> ${item.total} ${item.currency_code}</p>
+                    <p><b>Count:</b> ${item.products}</p>
+                    <p><b>Description: </b><span class="description"></span></p>
+                </div>
+                `)
+            })
+        })
+    .catch(error => console.error(error))
+
+    setTimeout(() => {
+        let cardItems = document.querySelectorAll(".card_item")
+
+        cardItems.forEach(cardItem => {
+            //console.log(dataObject.getOrderDataByID + '?id=' + item.querySelector(".id").textContent)
+            fetch(dataObject.getOrderDataByID + '?id=' + cardItem.querySelector(".id").textContent, {
+                method: 'GET',
+                headers: {
+                    'accept': 'application/json'
+                }
+            }).then(response => response.json())
+                .then((data) => {
+                    if((cardItem.querySelector(".id").textContent) === data.response.order_id) {
+                        console.log(cardItem.querySelector(".id").textContent, data.response.order_id)
+                        //console.log(data.response.products[0].name)
+                        document.querySelectorAll(".description").forEach((descriptionItem, i) => {
+                            console.log(descriptionItem)
+                            /*descriptionItem.insertAdjacentHTML("afterbegin", `<p class="items">${data.response.products[i]}</p>`)*/
+                        })
+                    }
+                })
+                .catch(error => console.error(error))
+        })
+    }, 5000)
+
+    /*let orderID = document.querySelectorAll
+    fetch(dataObject.getOrderDataByID + , {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        }
+    }).then(response => response.json())
+        .then((data) => {
+            (data.response).forEach(item => {
+                document.querySelector(".cards").insertAdjacentHTML("afterbegin", `
+                <div class="card_item">
+                    <h3>Order: № ${item.order_id}</h3>
+                    <hr>
+                    <p><b>Buyer:</b> ${item.name}</p>
+                    <p><b>Cost:</b> ${item.total} ${item.currency_code}</p>
+                    <p><b>Count:</b> ${item.products}</p>
+                    <p><b>Description: </b><span class="description"></span></p>
+                </div>
+                `)
+            })
+        })
+        .catch(error => console.error(error))*/
+}
+
---
+++ js/utils/dataObject.js
@@ -0,0 +1,8 @@
+let dataObject = {
+    getOrdersLink: 'https://sellavi-api.000webhostapp.com/101263/getOrders.php',
+    getOrderDataByID: 'https://sellavi-api.000webhostapp.com/101263/getOrderDataByID.php',
+    //key: '208c05-6582ab-a05185-442218-bb7c97',
+    orderID: '110026844'
+}
+
+export default dataObject
