ddf7132de8d6b7c79423b3507fd2c3dc70684ac5
Coospir <eugene.starodubov@gmail.com>
2020-12-28 14:20:22 +0300

    test

--- js/main/main.js
+++ js/main/main.js
@@ -1,21 +1,36 @@
 import dataObject from "../utils/dataObject.js"
 window.onload = () => {
+    let orders = {}
     fetch(dataObject.getOrdersLink, {
     }).then(response => response.json())
         .then((data) => {
-            (data.response).forEach(item => {
+            (data.response).forEach((item, index) => {
                 fetch(dataObject.getOrderDataByID + `?id=${item.order_id}`, {
                 }).then(response => response.json())
                     .then((data) => {
-                        console.log(data.response)
-                        document.querySelectorAll(".cards").forEach(item => {
-                            item.insertAdjacentHTML("afterbegin", `
-                            <div class="card_item">
-                                <p><span>Order №${data.response.order_id}</span></p>
-                                <span>Buyer: ${data.response.payment_lastname} ${data.response.payment_firstname}</span>
-                            </div>
-                        `)
-                        })
+                        if(item.order_id === data.response.order_id) {
+
+                            orders = {
+                                "id": data.response.order_id,
+                                "lastname": data.response.lastname,
+                                "firstname": data.response.firstname,
+                                "products": data.response.products,
+                                "cost": data.response.total
+                            }
+
+                            document.querySelectorAll(".cards").forEach((item, index) => {
+                                item.insertAdjacentHTML("afterbegin", `
+                                <div class="card_item">
+                                    <h3 class="title">Order №${orders.id}</h3>
+                                    <div class="buyer">Buyer: ${orders.lastname} ${orders.firstname}</div>
+                                    <div id=${orders.id} class="products">${orders.products}</div>
+                                    <div class="price">Price: ${orders.cost} rub.</div>
+                                </div>
+                            `)
+                            })
+                            console.log(Array.from(orders.products))
+                        }
+
                     })
             })
         })
